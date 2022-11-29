3bd94a65e1858ac67ff1e1fc3ce7b50ae2d9de73
Ethan Joseph <ethanj217@gmail.com>
2022-11-29 16:49:16 -0500

    complete experiment 3

--- experiment_3/gen_fake_data.py
+++ experiment_3/gen_fake_data.py
@@ -1,27 +1,67 @@
 import pandas as pd
 import numpy as np
 from faker import Faker
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import accuracy_score
 
 fake = Faker()
 
-if __name__ == '__main__':
-	df_census = pd.read_csv('data/New_York_NY.csv')
-	size = len(df_census)
-	names = [fake.name() for _ in range(size)]
-	addresses = [fake.address() for _ in range(size)]
-	df_census['name'] = names
-	df_census['loc'] = addresses
-	print(df_census.head())
-	le = LabelEncoder()
-	y = le.fit_transform(df_census['name'])
-	X = df_census[['REGION', 'P0050007', 'P0050008', 'GEOVAR']]
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+# simulate extra rows as if we had a very large dataset
+def augment_data(df):
+    num_features = len(df.columns)
+    a = np.arange(0, num_features)
+    aug_df = pd.DataFrame(index=df.index, columns=df.columns, dtype='float64')
 
-	model = LogisticRegression()
-	model.fit(X_train, y_train)
-	y_pred = model.predict(X_test)
-	print(accuracy_score(y_test, y_pred))
+    for i in range(0, len(df)):
+        AUG_FEATURE_RATIO = 0.5
+        AUG_FEATURE_COUNT = np.floor(
+            num_features*AUG_FEATURE_RATIO).astype('int16')
+        # randomly sample half of columns that will contain random values
+        aug_feature_index = np.random.choice(
+            num_features, AUG_FEATURE_COUNT, replace=False)
+        aug_feature_index.sort()
+
+        # obtain indices for features not in aug_feature_index
+        feature_index = np.where(np.logical_not(
+            np.in1d(a, aug_feature_index)))[0]
+
+        # first insert real values for features in feature_index
+        aug_df.iloc[i, feature_index] = df.iloc[i, feature_index]
+
+        # random row index to randomly sampled values for each features
+        rand_row_index = np.random.choice(
+            len(df), len(aug_feature_index), replace=True)
+
+        # for each feature being randomly sampled, extract value from random row in df
+        for n, j in enumerate(aug_feature_index):
+            aug_df.iloc[i, j] = df.iloc[rand_row_index[n], j]
+
+    return aug_df
+
+
+if __name__ == '__main__':
+    # augment health database
+    df = pd.read_csv('data/heart.csv')
+    for _ in range(5):
+        aug_df = augment_data(df)
+        df = pd.concat((df, aug_df))
+
+    df = df.drop('target', axis=1)
+    df.to_csv('data/health_database.csv', index=False)
+
+    # generate fake identified cross reference database
+    names = [fake.name_male() if x == 1 else fake.name_female()
+             for x in df['sex']]  # simulate names
+    locations = [fake.address() for _ in range(len(df))]  # simulate location
+    ssns = [fake.ssn() for _ in range(len(df))] # ssn
+
+    public_data = pd.DataFrame(list(zip(
+        df['sex'].to_list(),
+        df['age'].to_list(),
+        names,
+        locations,
+        ssns
+    )), columns=['sex', 'age', 'name', 'loc', 'ssn']).sample(frac=1)
+    public_data.to_csv('data/crossref_database.csv', index=False)
